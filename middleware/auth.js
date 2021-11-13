5525555cbd56bde8a0a3402072dd84fd490d2f5c
natanael-cahya <natanaelmc60@gmail.com>
2021-11-13 07:43:38 +0700

    Add register account code

---
+++ middleware/auth.js
@@ -0,0 +1,39 @@
+let connection = require('../db')
+let mysql = require('mysql')
+let md5 = require('md5')
+let response = require('../res')
+let jwt = require('jsonwebtoken')
+let config = require('../config/secret')
+let ip = require('ip')
+
+//register
+
+exports.regis = (req,res) => {
+    let post = {
+        email: req.body.email,
+        password: md5(req.body.password),
+        role: req.body.role,
+        tgl_daftar: new Date()
+    }
+
+    let query = `SELECT email FROM user WHERE email = '${post.email}'`
+    connection.query(query,(err,rows) => {
+        if(err) {
+            response.err(err,res)
+        }else{
+            if(rows.length == 0){
+                connection.query(`INSERT INTO user SET ${post}`, (err,rows) => {
+                    if(err){
+                        response.err('Gagal Daftar silahkan cek Ulang',res)
+                    }else{
+                        response.ok('Berhasil Daftar',res)
+                    }
+                })
+            }else{
+                response.err('Email Sudah terdaftar')
+            }
+        }
+    })
+
+}
+
